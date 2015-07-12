2d79f3dc87df8dd7f11e6a275057f6980a5b451a
TheStrix <parthbhatia98@gmail.com>
2015-07-12 06:31:29 -0400

    Update mr_init_devices

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -10,38 +10,24 @@ const char *mr_init_devices[] =
 	// Framebuffer
     "/sys/class/graphics/fb0", 
 
-	// storage devices
-    "/sys/block/mmcblk0",
-    "/sys/devices/msm_sdcc.1",
-    "/sys/devices/msm_sdcc.1/mmc_host/mmc0",
-    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
-    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
-    "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0*",
-    "/sys/bus/mmc",
-    "/sys/bus/mmc/drivers/mmcblk",
-    "/sys/module/mmc_core",
-    "/sys/module/mmcblk",
+	// Storage Devices
+    "/sys/devices/msm_sdcc.1*",
+    "/sys/devices/msm_sdcc.2*",
+    "/sys/devices/platform/msm_hsusb_host*",
+    "/sys/bus/mmc*",
+    "/sys/dev/block*",
 
-	// sdcard
-    "/sys/block/mmcblk1",
-    "/sys/devices/msm_sdcc.2/mmc_host*",
-
+	// Input
     "/sys/devices/gpio_keys.66/input*",
-    "/sys/class/input/event4",
-    "/sys/devices/virtual/input*",
     "/sys/devices/virtual/misc/uinput",
-    "/sys/class/misc/uinput",
+    "/sys/devices/virtual/input*",
     "/sys/devices/f9927000.i2c/i2c-5/5-0038/input*",
 
-	// for adb
+	// For adb
     "/sys/devices/virtual/tty/ptmx",
     "/sys/devices/virtual/misc/android_adb",
     "/sys/devices/virtual/android_usb/android0/f_adb",
     "/sys/bus/usb",
 
-	// USB drive is in here
-    "/sys/devices/platform/msm_hsusb_host*",
-
     NULL
 };
-
